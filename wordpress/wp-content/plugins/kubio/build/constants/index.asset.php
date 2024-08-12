71e460a193445f1df5cc81aefaae711250b0a9d6
rijojozeph <rijojozeph@gmail.com>
2024-08-12 13:17:01 +0300

    delete

--- wordpress/wp-content/plugins/kubio/build/constants/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('kubio-icons', 'wp-i18n', 'wp-polyfill'), 'version' => '1808450f11de1ae2cb27');
--- wordpress/wp-content/plugins/kubio/build/scripts/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'bb781e554637419a0f02');
